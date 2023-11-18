b457e260c92b153e6298e33b6e1546323a15fe75
cansufndk <canbugref@gmail.com>
2023-11-18 14:03:10 +0300

    first commit

---
+++ src/components/Todo/Todo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const Todo = () => {
+  return (
+    <SafeAreaView>
+        <Text>Todo</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Todo
---
+++ src/components/Todos/Todos.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+const Todos = () => {
+  return (
+    <SafeAreaView>
+        <Text>Todos</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Todos
